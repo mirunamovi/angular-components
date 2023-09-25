7b0d110885b5ecfb348b010375130b445365d800
mirunamovi <mirunamovi@gmail.com>
2023-09-25 20:03:39 +0300

    view and mods components

---
+++ comps/src/app/views/views-home/views-home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-views-home',
+  templateUrl: './views-home.component.html',
+  styleUrls: ['./views-home.component.css']
+})
+export class ViewsHomeComponent {
+  stats = [
+    {value: 22, label: '# of Users'},
+    {value: 900, label: 'Revenue'},
+    {value: 50, label: 'Reviews'}
+  ];
+
+  items = [
+    {image: '/assets/images/couch.jpeg', title: 'Couch', description: 'A Couch'},
+    {image: '/assets/images/dresser.jpeg', title: 'Dresser', description: 'A Dresser'},
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
